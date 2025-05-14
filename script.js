63d46aaa01c02134cc59c13bd2f29faad0c460bc
Jane Zhang <janezhang555l@gmail.com>
2025-05-14 09:58:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ document.getElementById('fetchData').addEventListener('click', function() {
         })
         .catch(error => console.error('Error:', error));
 })
+const projectsSection = document.querySelector('#projects');
+const logo = document.querySelector('.logo');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        logo.style.display = 'none'; // 隐藏 logo
+      } else {
+        logo.style.display = 'block'; // 显示 logo
+      }
+    });
+  },
+  {
+    threshold: 0.1, // 10% 出现在视口就触发
+  }
+);
+
+if (projectsSection) {
+  observer.observe(projectsSection);
+}
