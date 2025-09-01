4c953dd12725879e61a6c961ceeaa938fcca47c6
Jane Zhang <janezhang555l@gmail.com>
2025-09-02 02:54:29 +0800

    Add Traller project page with team info and update homepage with 4 project cards

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// 提供public目录下的静态文件
-app.use(express.static(path.join(__dirname, 'public')));
+// 提供根目录下的静态文件
+app.use(express.static(__dirname));
 
 // 提供media目录下的视频和图片
 app.use('/media', express.static(path.join(__dirname, 'media')));
